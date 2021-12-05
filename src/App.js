996506c290029163ec293d3876c729979d341162
tankamout <mountchitmyrsuu@gmail.com>
2021-12-05 21:34:54 +0630

    ui almost complete

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import Signup from './Screens/Signup'
 import ForgotPassword from './Components/ForgotPassword'
 import ResetPassword  from './Components/ResetPassword';
 import Cart from './Screens/Cart'
+import SingleProduct from './Screens/SingleProduct'
 
 function App() {
   return (
@@ -23,6 +24,7 @@ function App() {
           <Route  path="/" element={<Home/>} />
           <Route  path="/login" element={<Login/>}/>
           <Route path="/signup" element={<Signup/>} />
+          <Route path="/product/:id" element={<SingleProduct/>} />
           <Route path="/forgotpassword" element={<ForgotPassword/>} />
           <Route path="/resetpassword/:id/:token" element={<ResetPassword/>} />
           <Route  path="/cart" element={<Cart/>} />
--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Slider from '../Components/Slider'
 import Product from './Product'
 import Category  from '../Components/Category'
+import Footer from '../Components/Footer'
 const Home=()=>{
     return (
         <>
        <Slider/>
        <Category/>
         <Product/>
+        <Footer/>
         </>
     )
 }
